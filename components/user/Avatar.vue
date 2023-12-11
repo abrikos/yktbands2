9c4ccb93ea93b9c06107166f4c3266c03c7babad
Artem Filippow <a.filippov@qtech.ru>
2023-12-11 18:46:26 +0900

    avatar checks url

--- components/user/Avatar.vue
+++ components/user/Avatar.vue
@@ -2,11 +2,18 @@
 const props = defineProps({
     user: {type: Object, required: true}
 })
-
+const {user} = props
+const valid = ref()
+try {
+    const url = new URL(user.avatarImage)
+    valid.value = !!url
+}catch (e) {
+    
+}
 </script>
 
 <template lang="pug">
-img.avatar(:src="user.avatarImage || '/avatar.png'" onerror="this.src='/avatar.png'")
+img.avatar(:src="valid ? user.avatarImage : '/avatar.png'" onerror="this.src='/avatar.png'")
 </template>
 
 <style scoped lang="sass">
