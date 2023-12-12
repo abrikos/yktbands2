3efc57803c7fb34888738e44c95e648eea88d049
Artem Filippow <a.filippov@qtech.ru>
2023-12-13 02:56:18 +0900

    map optimization

--- components/band/Concerts.vue
+++ components/band/Concerts.vue
@@ -10,8 +10,7 @@ const snackbar = useSnackbar();
 const props = defineProps<{ band: IBand, places: IPlace[] }>()
 const {band, places} = props
 
-const defaultConcert = {bandId: band.id, placeId: null, coordinate: null as [number, number] | unknown, address: null as unknown, name: null as unknown, begin: 0}
-const newConcert = ref(defaultConcert)
+const newConcert = ref({bandId: band.id, placeId: null, coordinate: null as [number, number] | unknown, address: null as unknown, name: null as unknown, begin: 0})
 const newDate = ref<Date>()
 const bottomSheet = ref()
 const newHour = ref<number>(20)
@@ -39,20 +38,8 @@ function setHour(hour: number) {
 }
 
 async function addConcert() {
-    const type = 'warning'
-    const errors: string[] = []
-    const body = newConcert.value
-    body.placeId = selectedPlace.value?.id
-    if (!body.placeId) {
-        if (!body.coordinate) errors.push('Укажите ресторан на карте')
-        if (!body.name) errors.push('Укажите название ресторана')
-        if (!body.address) errors.push('Укажите адрес ресторана')
-    }
-    if (errors.length) {
-        errors.forEach((text: string) => snackbar.add({type, text}))
-        return
-    }
-    await useNuxtApp().$PUT('/concert/create', body)
+    newConcert.value.placeId = selectedPlace.value?.id
+    await useNuxtApp().$PUT('/concert/create', newConcert.value)
     emit('updateBand')
 }
 
@@ -62,9 +49,17 @@ function placeMarkerClick(place: IPlace) {
         return
     }
     selectedPlace.value = place
-    newConcert.value.name = place.name
-    newConcert.value.address = place.address
 }
+
+function placeCancel() {
+    selectedPlace.value = undefined
+}
+
+watch(selectedPlace,(v)=>{
+    newConcert.value.name = v?.name
+    newConcert.value.address = v?.address
+    newConcert.value.coordinate = v?.coordinate
+})
 
 const errorsRef = ref({coordinate: 'Укажите ресторан на карте', name: 'Укажите название ресторана', address: 'Укажите адрес ресторана', begin: 'Укажите дату концерта'})
 const errors = computed(() => {
@@ -73,7 +68,7 @@ const errors = computed(() => {
         errorsRef.value.coordinate = ''
         errorsRef.value.name = ''
         errorsRef.value.address = ''
-    }else{
+    } else {
         if (body.coordinate) errorsRef.value.coordinate = ''
         if (body.name) errorsRef.value.name = ''
         if (body.address) errorsRef.value.address = ''
@@ -94,37 +89,48 @@ div
                     ul
                         li(v-for="(concert,i) of band.concerts" :key="i") {{i}} {{concert.place?.name}} {{concert.place?.address}}
         v-col
-            v-bottom-sheet(v-model="bottomSheet" )
+            v-dialog(fullscreen            :scrim="false"                transition="dialog-bottom-transition")
                 template(v-slot:activator="{props}")
-                    v-btn(v-bind="props") ZZZZZ
-                v-card
-                    v-card-title Создание концерта
-                        small.text-blue.px-2
-                            span.px-1(v-if="newConcert.name") Ресторан "{{newConcert.name}}",
-                            span.px-1(v-if="newConcert.address") {{newConcert.address}},
-                            span.px-1(v-if="newConcert.begin") в {{moment.unix(newConcert.begin).format('YYYY-MM-DD HH:00')}}
-                        br
-                        v-btn(v-if="!Object.values(errors).filter(e=>!!e).length" @click="addConcert" color="primary") Создать концерт
-                    v-card-text
-                        v-combobox(v-if="!newConcert.coordinate" item-title="fullName" item-value="id" :items="places" v-model="selectedPlace" label="Выбрите существующий ресторан")
-                            template(v-slot:append)
-                                v-btn(v-if="selectedPlace" @click="selectedPlace = undefined" icon="mdi-cancel")
+                    v-btn(v-bind="props" color="primary" ) Добавить новый концерт
+                template( v-slot:default="{ isActive }")
+                    v-card
+                        v-card-text
+                            v-row
+                                v-col
+                                    v-card-title Создание концерта
+                                    v-divider
+                                    small Для выбора существующего ресторана кликните по маркеру. Для создания нового - кликните по карте
+                                    BandLeaflet(:cancel="placeCancel" :map-click="mapClick" :new-concert="newConcert" :selected-place="selectedPlace" :place-marker-click="placeMarkerClick" :places="places")
 
-                        v-text-field(v-if="!selectedPlace && !!newConcert.coordinate" v-model="newConcert.name" label="Введите название нового ресторана" :messages="errors.name" :error="!!errors.name")
-                            template(v-slot:append-inner)
-                                v-btn(v-if="newConcert.name" @click="newConcert.name = undefined" icon="mdi-cancel")
-                        span Для выбора существующего кликните по маркеру. Для создания нового кликните по дому когда курсор отображает руку раскрывшую 5 пальцев
-                        BandLeaflet(:map-click="mapClick" :new-concert="newConcert" :selected-place="selectedPlace" :place-marker-click="placeMarkerClick" :places="places")
-                        div.text-red {{errors.coordinate}}
-                        v-row
-                            v-col
-                                span Дата
-                                span.text-red.px-1 {{errors.begin}}
-                                v-date-picker.picker(v-model="newDate" hide-header show-adjacent-months title="zzz")
-                            v-col Время
-                                div.d-flex.flex-wrap
-                                    div.ma-1(v-for="hour of hours" :key="hour")
-                                        v-btn(@click="setHour(hour)" small :color="hour===newHour ? `secondary` : ''") {{hour > 9 ? hour : '0'+hour}}
+                                    v-combobox(placeholder="Поиск ресторана" item-title="fullName" item-value="id" :items="places" v-model="selectedPlace" density="compact" variant="outlined" )
+                                        template(v-slot:prepend-inner)
+                                            v-icon mdi-magnify
+                                        template(v-slot:append-inner)
+                                            v-btn(v-if="selectedPlace" @click="selectedPlace = undefined" icon="mdi-cancel")
+                                    div {{newConcert.address}}
+                                    v-text-field(placeholder="Введите название нового ресторана" v-if="!selectedPlace && !!newConcert.coordinate" v-model="newConcert.name" :messages="errors.name" :error="!!errors.name" variant="outlined" density="compact")
+                                        template(v-slot:append-inner)
+                                            v-btn(v-if="newConcert.name" @click="newConcert.name = undefined" icon="mdi-cancel")
+
+                                v-col(cols="5")
+                                    v-row
+                                        v-col
+                                            span Дата
+                                            span.text-red.px-1 {{errors.begin}}
+                                            v-date-picker.picker(v-model="newDate" hide-header show-adjacent-months title="zzz")
+                                        v-col
+                                            span Время
+                                            div.d-flex.flex-wrap
+                                                div.ma-1(v-for="hour of hours" :key="hour")
+                                                    v-btn(@click="setHour(hour)" small :color="hour===newHour ? `secondary` : ''") {{hour > 9 ? hour : '0'+hour}}
+                                    v-divider
+                                    div.text-blue.px-2
+                                        span.px-1(v-if="newConcert.name") Ресторан "{{newConcert.name}}",
+                                        span.px-1(v-if="newConcert.address") {{newConcert.address}},
+                                        span.px-1(v-if="newConcert.begin") {{moment.unix(newConcert.begin).format('YYYY-MM-DD HH:00')}}
+
+                                    v-btn(v-if="!Object.values(errors).filter(e=>!!e).length" @click="isActive.value = false; addConcert()" color="primary") Создать концерт
+                                    v-btn(@click="isActive.value = false") Закрыть
 
 
 </template>
--- components/band/Leaflet.vue
+++ components/band/Leaflet.vue
@@ -1,35 +1,65 @@
 <script setup lang="ts">
-//import L from 'leaflet'
 import type {IPlace} from "~/server/models/place.model";
 
-const props = defineProps<{ places: IPlace[], selectedPlace: IPlace, placeMarkerClick:(place:IPlace)=>void, mapClick:(e: any) => Promise<void>, newConcert:{}}>()
+const props = defineProps<{ places: IPlace[], selectedPlace: IPlace, placeMarkerClick: (place: IPlace) => void, mapClick: (e: any) => Promise<void>, cancel: (e: any) => void, newConcert: {} }>()
 
-const center = [62.02722510699265, 129.73493946155247]
-
-function test(i:number){
-    console.log(i)
+const center = ref([62.02722510699265, 129.73493946155247])
+const hoveredPlace = ref<IPlace>()
+const hoveredPosition = ref()
+function mapClickHandler(e: any) {
+    center.value = [e.latlng.lat, e.latlng.lng]
+    props.mapClick(e)
 }
 
+function placeMarkerClickHandler(place: IPlace) {
+    props.placeMarkerClick(place)
+    center.value = place.coordinate
+}
+
+function mouseOverHandle(e:any, place: IPlace) {
+    hoveredPlace.value = place
+    hoveredPosition.value =[e.originalEvent.clientX, e.originalEvent.clientY]
+}
+
+function mouseLeaveHandle(){
+    hoveredPlace.value = undefined
+}
+
+const popupStyle = computed(()=>{
+    const [left, top] = hoveredPosition.value ? hoveredPosition.value : [0,0]
+    return `top:${top}px;left:${left}px`
+    //return `top:${100}px;left:${100}px`
+})
 </script>
 
 <template lang="pug">
-div
-    div#leaflet
-        LMap(ref="map" :zoom="16" :center="center" @click="mapClick")
-            LTileLayer(url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="" layer-type="base" name="OpenStreetMap")
+client-only
+    div#popup(:style="popupStyle" v-if="hoveredPlace?.name") {{hoveredPlace?.name}}, {{hoveredPlace?.address}}
+    l-map#leaflet(ref="map" :zoom="16" :center="center" @click="mapClickHandler")
+        l-tile-layer(url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="" layer-type="base" name="OpenStreetMap")
+        l-control(position="bottomright")
+            v-btn(icon="mdi-cancel" @click="cancel")
+        l-marker(v-for="(place,i) of places.filter(p=>p.coordinateValid)" :lat-lng="place.coordinate" :key="i" @click="placeMarkerClickHandler(place)" @mouseover="(e)=>mouseOverHandle(e,place)" @mouseout="mouseLeaveHandle")
+            l-icon(v-if="selectedPlace?.id!==place.id" icon-url="/marker-blue.svg" :icon-anchor="[20,40]" )
+            l-icon(v-if="selectedPlace?.id===place.id" icon-url="/marker-red.svg" :icon-anchor="[20,40]" )
 
-            LMarker(v-for="(place,i) of places.filter(p=>p.coordinateValid)" :lat-lng="place.coordinate" :key="i" @click="placeMarkerClick(place)")
-                LIcon(v-if="selectedPlace?.id!==place.id" icon-url="/marker-blue.svg" :icon-anchor="[20,40]" )
-                LIcon(v-if="selectedPlace?.id===place.id" icon-url="/marker-red.svg" :icon-anchor="[20,40]" )
-
-            LMarker(v-if="newConcert.coordinate && !selectedPlace" :lat-lng="newConcert.coordinate")
-                LIcon(icon-url="/marker-red.svg" :icon-anchor="[20,40]" )
-
+        l-marker(v-if="newConcert.coordinate && !selectedPlace" :lat-lng="newConcert.coordinate")
+            l-icon(icon-url="/marker-red.svg" :icon-anchor="[20,40]" )
 
 
 </template>
 
 <style scoped lang="sass">
+#popup
+    -webkit-box-shadow: 4px 4px 7px 0px rgba(34, 60, 80, 0.19)
+    -moz-box-shadow: 4px 4px 7px 0px rgba(34, 60, 80, 0.19)
+    box-shadow: 4px 4px 7px 0px rgba(34, 60, 80, 0.19)
+    border-radius: 10px
+    padding: 10px
+    z-index: 100000000000
+    position: absolute
+    background-color: white
+    color: black
 #leaflet
     width: 100%
     height: 400px
