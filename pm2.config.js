f6d68295b97305811bc04af08d658c18b3a350e4
Artem Filippow <a.filippov@qtech.ru>
2023-12-20 22:52:59 +0900

    pm2 script

--- pm2.config.js
+++ pm2.config.js
@@ -1,21 +1,11 @@
-/**
- * Конфигурация для деплоя
- */
-export const apps = [{
-    name: `yktbands`,
-    script: 'node .output/server/index.mjs',
-    //node_args: '--preserve-symlinks -r esm',
-    env: {
-        NODE_ENV: 'production',
-        SOURCE_MAP: 'source-map',
-        NODE_PATH: '.',
-        //DEBUG: '*',
-    },
-    output: 'logs/backend.log',
-    error: 'logs/backend.log',
-    //log_date_format,
-    //combine_logs,
-    //error_file: `${__dirname}/logs/server.err.log`,
-    //out_file:   `${__dirname}/logs/server.out.log`,
-    //pid_file:   `${__dirname}/logs/server.pid`,
-}];
+module.exports = {
+    apps: [
+        {
+            name: 'NuxtAppName',
+            exec_mode: 'cluster',
+            instances: 'max',
+            script: './.output/server/index.mjs',
+            port: 5000
+        }
+    ]
+}
