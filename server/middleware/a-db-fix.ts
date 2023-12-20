475b8ea79492e04fc6d12f6a8d8e3385bf7cbb7d
Artem Filippow <a.filippov@qtech.ru>
2023-12-21 01:22:39 +0900

    fix db middleware for all models

--- server/middleware/a-db-fix.ts
+++ server/middleware/a-db-fix.ts
@@ -1,4 +1,10 @@
 import {User, Concert, Place, Band, Token, Artist, Instrument} from "#imports";
 export default defineEventHandler(async (event) => {
-
+    User.findOne()
+    Concert.findOne()
+    Place.findOne()
+    Band.findOne()
+    Token.findOne()
+    Artist.findOne()
+    Instrument.findOne()
 })
