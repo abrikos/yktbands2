7f236e818fb911fb3350bd20e426c1c7622aa1d3
Artem Filippow <a.filippov@qtech.ru>
2023-12-21 01:01:57 +0900

    fix schema

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,12 +1,11 @@
 import {IToken, Token} from "~/server/models/token.model";
-
+import {User} from "~/server/models/user.model";
 export default defineEventHandler(async (event) => {
     const {authExpiration, authTokenName, authRefreshBeforeExpiration} = useRuntimeConfig(event)
     const cookies = parseCookies(event)
     // @ts-ignore
     await Token.deleteExpiredTokens(authExpiration)
     const token: IToken | null = await Token.findOne({access: cookies[authTokenName]}).populate('user');
-    console.log(event.method, event._path)
     if (token) {
         if(authExpiration - token?.secondsFromCreation < authRefreshBeforeExpiration){
             console.log('Token refresh', authExpiration , token?.secondsFromCreation)
