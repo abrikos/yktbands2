c1916d499b2687b09992a11e8352c3f6b797691f
Artem Filippow <a.filippov@qtech.ru>
2023-12-23 20:49:19 +0900

    debug

--- server/utils/strategies.ts
+++ server/utils/strategies.ts
@@ -19,7 +19,7 @@ export const strategies:IStrategy = {
         const {username, first_name, last_name, photo_url} = body
         const email = username + '@telegram.org'
         const TOKEN: BinaryLike = process.env.BOT_TOKEN as BinaryLike;
-        //console.log(TOKEN)
+        console.log(TOKEN)
         const secret = crypto.createHash('sha256')
             .update(TOKEN)
             .digest();
